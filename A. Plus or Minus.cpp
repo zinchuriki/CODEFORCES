c947339c85b38c1e15c9e8c8e1a249fd173bd2cd
zinchuriki <priyanshu999.ps@gmail.com>
2025-05-16 13:03:31 +0530

    runnin' late

---
+++ A. Plus or Minus.cpp
@@ -0,0 +1,22 @@
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math,inline")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt;
+    cin >> tt;
+    while (tt--) {
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a+b==c)
+        cout<<'+'<<'\n';
+        else cout<< '-'<<'\n';
+        
+        
+    }
+return 0;
+
+}
