16c13a7b5e9b9b5a146c4ffb130c0013c7289699
zinchuriki <priyanshu999.ps@gmail.com>
2025-05-23 09:41:38 +0530

    Looking from a window above It's like a story of love

---
+++ A. Again Twenty Five!.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+   long long int n;
+   cin>>n;
+   cout<<25;
+    
+    return 0;
+}
