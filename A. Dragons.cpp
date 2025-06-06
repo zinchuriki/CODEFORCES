d5bf2a89f51648811adaa90f80f26918bb5a12c1
zinchuriki <priyanshu999.ps@gmail.com>
2025-06-06 23:43:17 +0530

    formatting

--- A. Dragons.cpp
+++ A. Dragons.cpp
@@ -1,41 +1,42 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    int s,n;
-    cin>>s>>n;
-    vector<pair<int,int>> vec;
 
-    pair<int,int> p;
-        int a,b;
-    for(int i=0; i<n; ++i){
-       cin>>a>>b;
-       p.first=a;
-       p.second=b;
-       vec.push_back(p); 
+    int s, n;
+    cin >> s >> n;
+    vector < pair < int, int >> vec;
+
+    pair < int, int > p;
+    int a, b;
+    for (int i = 0; i < n; ++i) {
+        cin >> a >> b;
+        p.first = a;
+        p.second = b;
+        vec.push_back(p);
     }
 
-    sort(vec.begin(),vec.end());
+    sort(vec.begin(), vec.end());
 
-    bool ans=true;
+    bool ans = true;
 
-    for(int i=0; i<vec.size(); ++i){
-        p=vec[i];
-        if(s<=p.first){
-ans=false;
-break;
+    for (int i = 0; i < vec.size(); ++i) {
+        p = vec[i];
+        if (s <= p.first) {
+            ans = false;
+            break;
         }
-        s+=p.second;
-        
+        s += p.second;
+
     }
 
-    if(ans)
-    cout<<"YES";
-    else cout<<"NO";
+    if (ans)
+        cout << "YES";
+    else cout << "NO";
 
-    
+
     return 0;
 }
