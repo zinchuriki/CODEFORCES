adbf3594d64be430545ac680d9e11337b6629bae
zinchuriki <priyanshu999.ps@gmail.com>
2025-06-09 20:12:36 +0530

    well ig intuition did the work

---
+++ A. Raising Bacteria.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+  long int n;
+  cin>>n;
+  if(n%2==0)
+  cout<<1;
+  else cout<<2;
+    
+    return 0;
+}
