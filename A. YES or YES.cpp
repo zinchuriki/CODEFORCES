74d3b0ef5ed0ad4dabb6c1699b8cea8104a3f54c
zinchuriki <priyanshu999.ps@gmail.com>
2025-07-05 00:26:59 +0530

    amount of inconsistency is ahh.

---
+++ A. YES or YES.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    // your code here
+    int tt;
+    cin>>tt;
+    while(tt--){
+string s;
+cin>>s;
+if(tolower(s[0])!='y'){
+cout<<"NO"<<'\n';
+continue;
+}
+if(tolower(s[1])!='e'){
+cout<<"NO"<<'\n';
+continue;
+}
+if(tolower(s[2])!='s'){
+cout<<"NO"<<'\n';
+continue;
+}
+
+cout<<"YES"<<'\n';
+
+        
+    }
+    
+    return 0;
+}
