c0d67a154aeae49a78afa2fd1ee0f4be57b8305b
zinchuriki <priyanshu999.ps@gmail.com>
2025-07-15 23:49:09 +0530

    drunk.

---
+++ B. Maximum Cost Deletion.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+int tt;
+cin>>tt;
+while(tt--){
+    int n,a,b;
+    cin>>n>>a>>b;
+
+    string s;
+    cin>>s;
+
+    if(a<0 && b<0)
+
+}
+    
+    return 0;
+}
