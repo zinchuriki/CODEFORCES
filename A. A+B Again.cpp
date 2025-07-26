e05fe576f731581abbb76579e32f96355163dfc4
zinchuriki <priyanshu999.ps@gmail.com>
2025-07-26 19:27:55 +0530

    back ahh.

---
+++ A. A+B Again.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+   int tt;
+   cin>>tt;
+   while(tt--){
+
+    string s;
+    cin>>s;
+    int a=s[0]-'0'+s[1]-'0';
+    cout<<a<<'\n';
+
+   }
+    
+    return 0;
+}
