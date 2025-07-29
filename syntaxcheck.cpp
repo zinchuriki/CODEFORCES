a42465878d579e7cbcb55d8a0ed942689fb6fea1
zinchuriki <priyanshu999.ps@gmail.com>
2025-07-29 23:14:32 +0530

    yup.

--- syntaxcheck.cpp
+++ syntaxcheck.cpp
@@ -1,48 +1,46 @@
-
 #pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math,inline")
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int main()
-{
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-  int n, k;
-  cin >> n >> k;
+   int n,w;
+    cin>>n>>w;
+    int a;
+    unordered_map<int,vector<int>> hash;
+    
+    for(int i=0; i<n; ++i){
+      cin>>a;
+      hash[a].push_back(i+1);
+    }
+    bool found=false;
+    for(auto it=hash.begin(); it!=hash.end(); ++it){
+      
+          vector<int> v=it->second;
+          
+          
+          if(v.size()>1){
+            cout<<v[0]<<" "<<v[1];
+            found=true;
+            break;
+          } else{
+                vector<int> v1=hash[w-v[0]];
+                if(v1.size()>0){
+                  cout<<v[0]<<" "<<v1[0];
+                  found =true;
+                  break;
+                }
+            
+          }
+      
+    }
+    if(!found) return -1;
+ 
 
-  unordered_map<int, int> hash;
 
-  int a;
-  for (int i = 0; i < n; ++i)
-  {
-    cin >> a;
-    hash[a]++;
-  }
+   return 0;
 
-  auto comp = [](pair<int, int> a, pair<int, int> b)
-  {
-    if (b.first > a.first)
-      return true;
-
-    if (b.second < a.second)
-      return true;
-
-    return false;
-  };
-
-  priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> pq(comp);
-
-  for (auto it = hash.begin(); it != hash.end(); ++it)
-    pq.push({it->second, it->first});
-  pair<int, int> p;
-  for (int i = 0; i < k; ++i)
-  {
-    p = pq.top();
-    pq.pop();
-    cout << p.second << " ";
-  }
-
-  return 0;
 }
