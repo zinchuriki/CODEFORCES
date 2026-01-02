0385a3d3604e907d6aa279229fdef044d57b7690
zinchuriki <priyanshu999.ps@gmail.com>
2026-01-02 11:31:02 +0530

    Adds solution to find repeated element
    Adds a solution to find the element that is repeated N times in an array.
    The solution uses a hash map to count the occurrences of each element and returns the element with a count greater than 1.

--- syntaxcheck.cpp
+++ syntaxcheck.cpp
@@ -7537,3 +7537,22 @@ public:
         }
     }
 };
+
+class Solution
+{
+public:
+    int repeatedNTimes(vector<int> &nums)
+    {
+        unordered_map<int, int> hash;
+        int n = nums.size();
+
+        for (int i = 0; i < n; ++i)
+        {
+            hash[nums[i]]++;
+            if (hash[nums[i]] > 1)
+                return nums[i];
+        }
+
+        return 0;
+    }
+};
