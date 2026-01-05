2e47bdadc6425f010e4a1dcc899427be77f5b8c2
zinchuriki <priyanshu999.ps@gmail.com>
2026-01-05 21:28:39 +0530

    Adds solution for maximum matrix sum
    Adds a solution to calculate the maximum sum achievable from a given matrix.
    The algorithm considers the number of negative elements and minimizes absolute values when the count of negatives is odd.

--- syntaxcheck.cpp
+++ syntaxcheck.cpp
@@ -7672,3 +7672,31 @@ public:
         return ans;
     }
 };
+
+class Solution
+{
+public:
+    long long maxMatrixSum(vector<vector<int>> &matrix)
+    {
+        int cnt = 0;
+        int mini = INT_MAX;
+        int n = matrix.size();
+        int ans = 0;
+
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (matrix[i][j] < 0)
+                    cnt++;
+                mini = min(mini, abs(matrix[i][j]));
+                ans += abs(matrix[i][j]);
+            }
+        }
+
+        if (cnt % 2 == 0)
+            return ans;
+        else
+            return ans - abs(mini);
+    }
+};
