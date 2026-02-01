d8bf83da7f3e5f02db5ca3e2ae4e47047f602440
zinchuriki <priyanshu999.ps@gmail.com>
2026-02-01 22:38:20 +0530

    Minimize cost by selecting smallest valid subarray starts
    Fix first subarray cost as nums[0]
    
    Find two smallest elements in the remaining array
    
    Sum these three values to get the minimum total cost

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -8515,6 +8515,31 @@ public:
                 pq.push({dist[next], next});
             }
         }
-        return -1
+        return -1;
+    }
+};
+
+class Solution
+{
+public:
+    int minimumCost(vector<int> &nums)
+    {
+        int first = nums[0];
+        int mn1 = INT_MAX, mn2 = INT_MAX;
+
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] < mn1)
+            {
+                mn2 = mn1;
+                mn1 = nums[i];
+            }
+            else if (nums[i] < mn2)
+            {
+                mn2 = nums[i];
+            }
+        }
+
+        return first + mn1 + mn2;
     }
 };
