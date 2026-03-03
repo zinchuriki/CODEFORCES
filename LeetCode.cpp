827f0aeceb9daf994a8c5629bbd82ae27916b1b2
zinchuriki <priyanshu999.ps@gmail.com>
2026-03-03 22:51:38 +0530

    todays lc.

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -9482,3 +9482,31 @@ public:
         return ans;
     }
 };
+
+class Solution
+{
+public:
+    char findKthBit(int n, int k)
+    {
+        if (n == 1)
+            return '0';
+
+        int len = (1 << n) - 1;
+        int mid = len / 2 + 1;
+
+        if (k == mid)
+        {
+            return '1';
+        }
+        else if (k < mid)
+        {
+            return findKthBit(n - 1, k);
+        }
+        else
+        {
+            int symmetric_k = len - k + 1;
+            char bit = findKthBit(n - 1, symmetric_k);
+            return (bit == '0') ? '1' : '0';
+        }
+    }
+};
