1c313122da3b0bbe450891f5973a0eb5a3648806
zinchuriki <priyanshu999.ps@gmail.com>
2026-04-05 22:09:53 +0530

    Add C++ solution for LeetCode 657: Robot Return to Origin

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -10219,3 +10219,35 @@ public:
         return ans_final;
     }
 };
+
+class Solution
+{
+public:
+    bool judgeCircle(string moves)
+    {
+        int x = 0;
+        int y = 0;
+
+        for (char move : moves)
+        {
+            if (move == 'U')
+            {
+                y++;
+            }
+            else if (move == 'D')
+            {
+                y--;
+            }
+            else if (move == 'R')
+            {
+                x++;
+            }
+            else if (move == 'L')
+            {
+                x--;
+            }
+        }
+
+        return x == 0 && y == 0;
+    }
+};
