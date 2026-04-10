a9914df1c2d2c3ba519132b8c7b8868743eb8a8f
Priyanshu Sharma <priyanshu999.ps@gmail.com>
2026-04-10 15:33:23 +0530

    Add C++ solution for minimum distance problem in LeetCode

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -10439,6 +10439,30 @@ public:
             ans.push_back(abs(findClosest(temp, nums[i]) - nums[i]));
 
         return ans;
-        
+    }
+};
+
+class Solution
+{
+public:
+    int minimumDistance(vector<int> &nums)
+    {
+        int ans = INT_MAX;
+
+        int n = nums.size();
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = i + 1; j < n; ++j)
+            {
+                for (int k = j + 1; k < n; ++k)
+                {
+                    if (nums[i] == nums[j] && nums[j] == nums[k])
+                    {
+                        ans = min(ans, abs(i - j) + abs(j - k) + abs(k - i));
+                    }
+                }
+            }
+        }
+        return ans;
     }
 };
