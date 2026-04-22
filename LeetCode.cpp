f1499a14e0ef1ae17660bfb0cf7f5d62b3d6f5bd
Priyanshu Sharma <priyanshu999.ps@gmail.com>
2026-04-22 11:12:55 +0530

    Add implementation of twoEditWords function to compare queries with dictionary

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -10585,3 +10585,35 @@ public:
         return ans;
     }
 };
+
+class Solution
+{
+public:
+    vector<string> twoEditWords(vector<string> &queries,
+                                vector<string> &dictionary)
+    {
+        vector<string> ans;
+        int n = queries.size();
+        int m = dictionary.size();
+        int l = queries[0].size();
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                int cnt = 0;
+
+                for (int k = 0; k < l; ++k)
+                {
+                    if (queries[i][k] != dictionary[j][k])
+                        cnt++;
+                }
+                if (cnt <= 2)
+                {
+                    ans.push_back(queries[i]);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
