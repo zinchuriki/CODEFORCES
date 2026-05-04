25c82e63eb45cac15dddcb985626dd600988612d
Priyanshu Sharma <priyanshu999.ps@gmail.com>
2026-05-04 19:54:09 +0530

    Add implementation of rotate function to rotate a matrix

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -10956,3 +10956,25 @@ public:
         return false;
     }
 };
+
+class Solution
+{
+public:
+    void rotate(vector<vector<int>> &matrix)
+    {
+        int n = matrix.size();
+        vector<vector<int>> vec;
+
+        for (int j = 0; j < n; ++j)
+        {
+            vector<int> temp;
+            for (int i = n - 1; i >= 0; --i)
+            {
+                temp.push_back(matrix[i][j]);
+            }
+            vec.push_back(temp);
+        }
+
+        matrix = vec;
+    }
+};
