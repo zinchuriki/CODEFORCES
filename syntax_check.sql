0437375999551c48424156573a7b6e8562009bad
Priyanshu Sharma <priyanshu999.ps@gmail.com>
2026-05-09 11:45:30 +0530

    Add SQL table creation for cloneListPro with relevant fields

--- syntax_check.sql
+++ syntax_check.sql
@@ -8,4 +8,19 @@ select customer_id, product_key
 ) AS customer_id
 GROUP BY customer_id
 HAVING COUNT(*)=(SELECT COUNT(*)
-from Product)
+from Product);
+
+
+
+CREATE TABLE cloneListPro(
+
+item_id INT PRIMARY KEY,
+product_id INT,
+pname VARCHAR(255),
+inStock CHAR(1),
+price DOUBLE,
+arrival_days INT,
+seller VARCHAR(255),
+youSave DOUBLE
+
+);
