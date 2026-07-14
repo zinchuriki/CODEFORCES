48869610d66f87d16fca472fdb86bd994060f5fb
Priyanshu Sharma <priyanshu999.ps@gmail.com>
2026-07-14 15:59:27 +0530

    Add Solution class with method to count subsequence pairs with equal GCD

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -15399,3 +15399,44 @@ public:
         return ans;
     }
 };
+
+class Solution
+{
+public:
+    int MOD = 1e9 + 7;
+    int dp[200][201][201];
+    int n;
+
+    int cal(vector<int> &nums, int gcd1, int gcd2, int idx)
+    {
+
+        if (idx == n)
+        {
+
+            if (gcd1 == gcd2 && gcd1 > 0)
+            {
+                return 1;
+            }
+            return 0;
+        }
+
+        if (dp[idx][gcd1][gcd2] != -1)
+            return dp[idx][gcd1][gcd2];
+
+        int take1 = cal(nums, std::gcd(gcd1, nums[idx]), gcd2, idx + 1);
+
+        int take2 = cal(nums, gcd1, std::gcd(gcd2, nums[idx]), idx + 1);
+
+        int dtake = cal(nums, gcd1, gcd2, idx + 1);
+
+        return dp[idx][gcd1][gcd2] = ((take1 + take2) % MOD + dtake) % MOD;
+    }
+
+    int subsequencePairCount(vector<int> &nums)
+    {
+        memset(dp, -1, sizeof(dp));
+        int ans = 0;
+        n = nums.size();
+        return cal(nums, 0, 0, 0);
+    }
+};
